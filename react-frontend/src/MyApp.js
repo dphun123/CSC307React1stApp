32d3d0a86a74005edad59869266b12fe5fb45235
dphun123 <dennisphun321@gmail.com>
2023-10-08 17:26:37 -0700

    6

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -13,8 +13,11 @@ function MyApp() {
   setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 200)
+       setCharacters([...characters, person] );
+    });
   }
 
   async function fetchAll(){
@@ -27,7 +30,18 @@ function MyApp() {
        console.log(error); 
        return false;         
     }
- }
+  }
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:8000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
 
   useEffect(() => {
     fetchAll().then( result => {
