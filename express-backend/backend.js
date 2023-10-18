2f9de8935c6ede39495a83a79fbd77ed6db54356
dphun123 <dennisphun321@gmail.com>
2023-10-18 11:05:15 -0700

    Revert "added database access on backend"
    This reverts commit 3d81eb7ffdc30b75a6935ce00b2ac15e27d8204a.

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -1,57 +1,111 @@
 import express from "express";
-import cors from "cors";
-
-import userServices from "./models/user-services.js";
+import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = 8000;
 
+const users = { 
+  users_list :
+  [
+     { 
+        id : 'xyz789',
+        name : 'Charlie',
+        job: 'Janitor',
+     },
+     {
+        id : 'abc123', 
+        name: 'Mac',
+        job: 'Bouncer',
+     },
+     {
+        id : 'ppp222', 
+        name: 'Mac',
+        job: 'Professor',
+     }, 
+     {
+        id: 'yat999', 
+        name: 'Dee',
+        job: 'Aspring actress',
+     },
+     {
+        id: 'zap555', 
+        name: 'Dennis',
+        job: 'Bartender',
+     }
+  ]
+}
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/users", async (req, res) => {
-  const name = req.query["name"];
-  const job = req.query["job"];
-  try {
-    const result = await userServices.getUsers(name, job);
-    res.send({ users_list: result });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("An error ocurred in the server.");
+app.get('/users', (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job
+  if (name != undefined && job!= undefined){
+      let result = findUserByNameAndJob(name, job)
+      result = {users_list: result};
+      res.send(result);
+  }
+  if (name != undefined){
+      let result = findUserByName(name);
+      result = {users_list: result};
+      res.send(result);
+  }
+  else{
+      res.send(users);
   }
 });
 
-app.get("/users/:id", async (req, res) => {
-  const id = req.params["id"];
-  const result = await userServices.findUserById(id);
-  if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
-  else {
-    res.send({ users_list: result });
-  }
-});
+const findUserByName = (name) => { 
+  return users['users_list'].filter( (user) => user['name'] === name); 
+}
 
-app.post("/users", async (req, res) => {
-  const user = req.body;
-  const savedUser = await userServices.addUser(user);
-  if (savedUser) res.status(201).send(savedUser);
-  else res.status(500).end();
-});
+const findUserByNameAndJob = (name, job) => { 
+  return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job); 
+}
 
-app.delete('/users/:id', async (req, res) => {
+app.get('/users/:id', (req, res) => {
   const id = req.params['id']; //or req.params.id
-  const result = await userServices.deleteUserById(id);
-  if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
   else {
-    res.status(204).end()
+      result = {users_list: result};
+      res.send(result);
+  }
+});
+
+function findUserById(id) {
+  return users['users_list'].find( (user) => user['id'] === id); // or line below
+  //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post('/users', (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(201).send(userToAdd);
+});
+
+function addUser(user){
+  if (!user['id']){
+    user['id'] = uuidv4();
+  }
+  users['users_list'].push(user);
+}
+
+app.delete('/users/:id', (req, res) => {
+  const id = req.params['id']; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
+  else {
+      const indexUser = users['users_list'].indexOf(result)
+      users['users_list'].splice(indexUser, 1)
+      res.status(204).end()
   }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`);
+});    
